c1fa4643ea11ccd2bb68c46c42fd53dcd2b6222d
ClaudeirZ2 <165345747+ClaudeirZ2@users.noreply.github.com>
2024-06-10 19:07:24 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import wppconnect from '@wppconnect-team/wppconnect';
-
 import dotenv from 'dotenv';
 import { initializeNewAIChatSession, mainOpenAI } from './service/openai';
 import { splitMessages, sendMessagesWithDelay } from './util';
@@ -14,8 +13,8 @@ const AI_SELECTED: AIOption = (process.env.AI_SELECTED as AIOption) || 'GEMINI';
 const MAX_RETRIES = 3;
 
 if (AI_SELECTED === 'GEMINI' && !process.env.GEMINI_KEY) {
-  throw Error(
-    'Você precisa colocar uma key do Gemini no .env! Crie uma gratuitamente em https://aistudio.google.com/app/apikey?hl=pt-br'
+  throw new Error(
+    'Você precisa colocar uma chave do Gemini no .env! Crie uma gratuitamente em https://aistudio.google.com/app/apikey?hl=pt-br'
   );
 }
 
@@ -23,8 +22,8 @@ if (
   AI_SELECTED === 'GPT' &&
   (!process.env.OPENAI_KEY || !process.env.OPENAI_ASSISTANT)
 ) {
-  throw Error(
-    'Para utilizar o GPT você precisa colocar no .env a sua key da openai e o id do seu assistante.'
+  throw new Error(
+    'Para utilizar o GPT você precisa colocar no .env a sua chave da OpenAI e o ID do seu assistente.'
   );
 }
 
@@ -32,96 +31,96 @@ wppconnect
   .create({
     session: 'sessionName',
     catchQR: (base64Qrimg, asciiQR, attempts, urlCode) => {
-      console.log('Terminal qrcode: ', asciiQR);
+      console.log('Terminal QR Code:', asciiQR);
     },
     statusFind: (statusSession, session) => {
-      console.log('Status Session: ', statusSession);
-      console.log('Session name: ', session);
+      console.log('Status da Sessão:', statusSession);
+      console.log('Nome da Sessão:', session);
     },
-    headless: 'new' as any,
+    headless: false // Alterado para false
   })
   .then((client) => {
     start(client);
   })
-  .catch((erro) => {
-    console.log(erro);
+  .catch((error) => {
+    console.error('Erro ao iniciar o cliente:', error);
   });
 
 async function start(client: wppconnect.Whatsapp): Promise<void> {
-  client.onMessage((message) => {
-    (async () => {
-      if (
-        message.type === 'chat' &&
-        !message.isGroupMsg &&
-        message.chatId !== 'status@broadcast'
-      ) {
-        const chatId = message.chatId;
-        console.log('Mensagem recebida:', message.body);
-        if (AI_SELECTED === 'GPT') {
-          await initializeNewAIChatSession(chatId);
-        }
-
-        if (!messageBufferPerChatId.has(chatId)) {
-          messageBufferPerChatId.set(chatId, [message.body]);
-        } else {
-          messageBufferPerChatId.set(chatId, [
-            ...messageBufferPerChatId.get(chatId),
-            message.body,
-          ]);
-        }
-
-        if (messageTimeouts.has(chatId)) {
-          clearTimeout(messageTimeouts.get(chatId));
-        }
-        console.log('Aguardando novas mensagens...');
-        messageTimeouts.set(
-          chatId,
-          setTimeout(() => {
-            (async () => {
-              const currentMessage = !messageBufferPerChatId.has(chatId)
-                ? message.body
-                : [...messageBufferPerChatId.get(chatId)].join(' \n ');
-              let answer = '';
-              
-              // Indicando que estamos digitando
-              client.startTyping(chatId).catch(console.error);
-      
-              for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
-                try {
-                  if (AI_SELECTED === 'GPT') {
-                    answer = await mainOpenAI({
-                      currentMessage,
-                      chatId,
-                    });
-                  } else {
-                    answer = await mainGoogle({
-                      currentMessage,
-                      chatId,
-                    });
-                  }
-                  break;
-                } catch (error) {
-                  if (attempt === MAX_RETRIES) {
-                    throw error;
-                  }
-                }
-              }
-              // Parando a indicação de que estamos digitando
-              client.stopTyping(chatId).catch(console.error);
-              
-              const messages = splitMessages(answer);
-              console.log('Enviando mensagens...');
-              await sendMessagesWithDelay({
-                client,
-                messages,
-                targetNumber: message.from,
-              });
-              messageBufferPerChatId.delete(chatId);
-              messageTimeouts.delete(chatId);
-            })();
-          }, 15000)
-        );
+  client.onMessage(async (message) => {
+    if (
+      message.type === 'chat' &&
+      !message.isGroupMsg &&
+      message.chatId !== 'status@broadcast'
+    ) {
+      const chatId = message.chatId;
+      console.log('Mensagem recebida:', message.body);
+      if (AI_SELECTED === 'GPT') {
+        await initializeNewAIChatSession(chatId);
       }
-    })();
+
+      if (!messageBufferPerChatId.has(chatId)) {
+        messageBufferPerChatId.set(chatId, [message.body]);
+      } else {
+        messageBufferPerChatId.set(chatId, [
+          ...messageBufferPerChatId.get(chatId),
+          message.body,
+        ]);
+      }
+
+      if (messageTimeouts.has(chatId)) {
+        clearTimeout(messageTimeouts.get(chatId));
+      }
+
+      console.log('Aguardando novas mensagens...');
+      messageTimeouts.set(
+        chatId,
+        setTimeout(async () => {
+          const currentMessage = !messageBufferPerChatId.has(chatId)
+            ? message.body
+            : [...messageBufferPerChatId.get(chatId)].join(' \n ');
+          let answer = '';
+
+          // Indicando que estamos digitando
+          client.startTyping(chatId).catch(console.error);
+
+          for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+            try {
+              if (AI_SELECTED === 'GPT') {
+                answer = await mainOpenAI({
+                  currentMessage,
+                  chatId,
+                });
+              } else {
+                answer = await mainGoogle({
+                  currentMessage,
+                  chatId,
+                });
+              }
+              break;
+            } catch (error) {
+              if (attempt === MAX_RETRIES) {
+                throw error;
+              }
+            }
+          }
+
+          // Parando a indicação de que estamos digitando
+          client.stopTyping(chatId).catch(console.error);
+
+          const messages = splitMessages(answer);
+          console.log('Enviando mensagens...');
+          await sendMessagesWithDelay({
+            client,
+            messages,
+            targetNumber: message.from,
+          });
+
+          messageBufferPerChatId.delete(chatId);
+          messageTimeouts.delete(chatId);
+        }, 15000)
+      );
+    }
   });
 }
+
